aa8f56375bda5df0ac95b57f295e8cade89a4262
Matthew L Davis <mld.0@protonmail.com>
2022-08-22 22:35:44 +1000

    05-c-operators

---
+++ 05-c-operators/c-operators.c
@@ -0,0 +1,59 @@
+
+//	Arithemtic
+//		+		Add
+//		-		Subtract
+//		*		Multiply
+//		/		Divide
+//		%		Modulus
+//		++		Increment
+//		--		Decrement
+
+//	Relational:
+//		==		Equal
+//		!=		Not-equal
+//		>		Greater-than
+//		<		Less-than
+//		>=		Greater-than-equal-to
+//		<=		Less-than-equal-to
+
+//	Logical:
+//		&&		And
+//		||		Or
+//		!		Not
+//		? :		Ternary
+
+//	Bitwise:
+//		&		And
+//		|		Or
+//		^		Xor
+//		~		Ones-complement
+//		<<		Shift-left
+//		>>		Shift-right
+
+//	Assigment:
+//		=		Equal
+//		+=		Plus-equal
+//		-=		Minus-equal
+//		*=		Multiply-equal
+//		/=		Divide-equal
+//		%=		Modulus-equal
+//		<<=		Shift-left-equal
+//		>>=		Shift-right-equal
+//		&=		And-equal
+//		^=		Xor-equal
+//		|=		Or-equal
+
+//	Data operators:
+//		sizeof()		Get size
+//		[]				Array subscript
+//		&				Address of
+//		*				Value of
+//		->				Structure dereference
+//		.				Structure reference
+
+//	Misc:
+//		,				Comma
+//		()				Parentheses
+//		{}				Braces
+//		:				Colon
+
