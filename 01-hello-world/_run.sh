0eb54728ff44581be3047ef074bfa693e270839f
Matthew L Davis <mld.0@protonmail.com>
2022-08-22 21:51:36 +1000

    01-hello-world
    Compiling source file using make without makefile example.

---
+++ 01-hello-world/_run.sh
@@ -0,0 +1,9 @@
+
+#	Can ask make to build a file without needing a makefile:
+make hello-world
+#	Runs command:
+#>	cc     hello-world.c   -o hello-world	
+
+#	Run the resulting binary
+./hello-world
+
