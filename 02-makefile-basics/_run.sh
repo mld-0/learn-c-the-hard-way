d5a60b87a4971e57e63873ade841bae16a61c56f
Matthew L Davis <mld.0@protonmail.com>
2022-08-22 22:01:35 +1000

    makefile with run script
    makefile provides 'clean' (deletes binary), and sets CFLAGS for any
    make-ing in this directory.

---
+++ 02-makefile-basics/_run.sh
@@ -0,0 +1,11 @@
+
+#	Tell make to build 'hello-world'
+#	CFLAGS from makefile will be used
+make hello-world
+
+#	Run binary
+./hello-world
+
+#	makefile also provides 'clean' command (deletes binary)
+make clean
+
