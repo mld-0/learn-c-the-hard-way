a82febca91703d7a3befe3b5eeecde850aa77021
Matthew L Davis <mld.0@protonmail.com>
2022-08-22 21:55:51 +1000

    add CFLAGS

--- 01-hello-world/_run.sh
+++ 01-hello-world/_run.sh
@@ -1,8 +1,9 @@
 
-#	Can ask make to build a file without needing a makefile:
-make hello-world
-#	Runs command:
-#>	cc     hello-world.c   -o hello-world	
+#	Can ask make to build a file without needing a makefile (and provide CFLAGS).
+CFLAGS="-Wall" make hello-world
+#	Does 'hello-world' exist already?
+#	If not, is there a source file beginning with 'hello-world'
+#	Run: 'cc hello-world.c -o hello-world'
 
 #	Run the resulting binary
 ./hello-world
