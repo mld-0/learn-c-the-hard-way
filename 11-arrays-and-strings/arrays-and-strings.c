b72e602bfabeca95b3c5326b31267ecafe12952f
Matthew L Davis <mld.0@protonmail.com>
2022-08-25 00:13:09 +1000

    Use 'const char*' for literal C-strings
    Ongoing is printing (the address of a) pointer with printf (as hex)
    without a wrong-data-type warning.

--- 11-arrays-and-strings/arrays-and-strings.c
+++ 11-arrays-and-strings/arrays-and-strings.c
@@ -1,5 +1,15 @@
+//	{{{3
+//	vim: set tabstop=4 modeline modelines=10:
+//	vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+//	{{{2
 #include <stdio.h>
 #include <string.h>
+//	Ongoings:
+//	{{{
+//	Ongoing: 2022-08-25T00:07:09AEST attempting to modify a C-string assigned to 'char*' produces a crash with (no error seen in vim-exe) 'bus error' 
+//	Ongoing: 2022-08-25T00:08:41AEST Address of a literal C-string assigned to 'char*' vs assigned to 'char[]' -> '0x100923f84' vs '0x16d531dbc'
+//	Ongoing: 2022-08-25T00:10:31AEST Printing a pointer value with 'printf("%lx\n", cstr1)' (where cstr1 is a char*) -> a pointer is a 64bit value (so how do I make the wrong-data-type-warning go away?)
+//	}}}
 
 //	A C-string is an array of bytes, termianted with '\0' (null-byte).
 //	(This can be very problematic).
@@ -17,8 +27,9 @@ int main()
 	printf("name=(%s)\n", name);
 
 	//	Book recomendation: use 'char* x' not 'char x[]' for C-strings initalized with quotes
+	//	Stackoverflow recomendation: Use 'const char*' for C-string literals
 	//	(The resulting string will be read only)
-	//	(can't use sizeof(x)/sizeof(*x) on a pointer)
+	//	(can't use sizeof(x)/sizeof(*x) on a pointer) (see item 15 for arrays vs pointers)
 	char* cstr1 = "asdf";
 	int len_cstr1 = strlen(cstr1);
 	printf("len_cstr1=(%i)\n", len_cstr1);
